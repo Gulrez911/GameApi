e8b4d08b03624924eb3a7ddc139272e823cedd5d
Gulrez <gulfarooqui1@gmail.com>
2019-05-30 15:22:01 +0530

    saveUserGame done

--- src/main/java/com/kgate/controller/UserController.java
+++ src/main/java/com/kgate/controller/UserController.java
@@ -6,6 +6,9 @@ import com.kgate.exception.ResourceNotFoundExceptionTest;
 import com.kgate.repository.UserRepository;
 import com.kgate.service.UserService;
 import java.io.PrintStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
@@ -31,11 +34,177 @@ public class UserController {
 	}
 
 	@PostMapping({ "/saveUserGame" })
-	public ResponseEntity<?> saveUserGame(@RequestBody UserGame userGame) {
+	public ResponseEntity<?> saveUserGame(@RequestBody String json) {
 		System.out.println("save called");
+		JSONObject jsonObject = new JSONObject(json);
+		JSONObject getObject = jsonObject.getJSONObject("MainGameData");
+		JSONArray getArray = jsonObject.getJSONArray("QuestionData");
+
+		String phone = (String) getObject.get("phoneNumber");
+		String feedback = (String) getObject.get("feedback");
+		String duration = (String) getObject.get("Duration");
+		String dateTime = (String) getObject.get("DateTime");
+		String percentage = (String) getObject.get("Percentage");
+		System.out.println("done: " + phone + " " + feedback + " " + duration + " " + dateTime + " "
+				+ percentage);
+		UserGame q2 = new UserGame();
+//		for (int i = 0; i < getArray.length(); i++) {
+		JSONObject objects1 = getArray.getJSONObject(0);
+
+//			Question q2 = new Question();
+		String objective1 = (String) objects1.get("objective");
+		String qNo1 = (String) objects1.get("qNo");
+		String question1 = (String) objects1.get("question");
+		String option1 = (String) objects1.get("option");
+		String result1 = (String) objects1.get("result");
+		String type1 = (String) objects1.get("type");
+		System.out.println("question: " + objective1 + " " + qNo1 + " " + question1 + " " + option1 + " "
+				+ result1 + " " + type1);
+		q2.setMobile(phone);
+		q2.setFeedback(feedback);
+		q2.setDuration(duration);
+		q2.setDateTime(dateTime);
+		q2.setPercentage(percentage);
+		q2.setObjective(objective1);
+		q2.setqNo1(Integer.parseInt(qNo1));
+		q2.setQ1(question1);
+		q2.setQa1(option1);
+		q2.setRestult1(result1);
+		q2.setType1(type1);
+
+		JSONObject objects2 = getArray.getJSONObject(1);
+
+//			Question q2 = new Question();
+		String objective2 = (String) objects2.get("objective");
+		String qNo2 = (String) objects2.get("qNo");
+		String question2 = (String) objects2.get("question");
+		String option2 = (String) objects2.get("option");
+		String result2 = (String) objects2.get("result");
+		String type2 = (String) objects2.get("type");
+		System.out.println("question: " + objective1 + " " + qNo2 + " " + question2 + " " + option2 + " "
+				+ result1 + " " + type1);
+//			q2.setObjective(objective);(objective);
+		q2.setqNo2(Integer.parseInt(qNo2));
+		q2.setQ2(question2);
+		q2.setQa2(option2);
+		q2.setRestult2(result2);
+		q2.setType2(type2);
+
+		JSONObject objects3 = getArray.getJSONObject(2);
+
+//			Question q2 = new Question();
+		String objective3 = (String) objects3.get("objective");
+		String qNo3 = (String) objects3.get("qNo");
+		String question3 = (String) objects3.get("question");
+		String option3 = (String) objects3.get("option");
+		String result3 = (String) objects3.get("result");
+		String type3 = (String) objects3.get("type");
+		System.out.println("question: " + objective3 + " " + qNo3 + " " + question3 + " " + option3 + " "
+				+ result3 + " " + type3);
+		q2.setqNo3(Integer.parseInt(qNo3));
+		q2.setQ3(question3);
+		q2.setQa3(option3);
+		q2.setRestult3(result3);
+		q2.setType3(type3);
+
+		JSONObject objects4 = getArray.getJSONObject(3);
+
+//			Question q2 = new Question();
+		String objective4 = (String) objects4.get("objective");
+		String qNo4 = (String) objects4.get("qNo");
+		String question4 = (String) objects4.get("question");
+		String option4 = (String) objects4.get("option");
+		String result4 = (String) objects4.get("result");
+		String type4 = (String) objects4.get("type");
+		System.out.println("question: " + objective4 + " " + qNo4 + " " + question4 + " " + option4 + " "
+				+ result4 + " " + type4);
+		q2.setqNo4(Integer.parseInt(qNo4));
+		q2.setQ4(question4);
+		q2.setQa4(option4);
+		q2.setRestult4(result4);
+		q2.setType4(type4);
+
+		JSONObject objects5 = getArray.getJSONObject(4);
+
+//			Question q2 = new Question();
+		String objective5 = (String) objects5.get("objective");
+		String qNo5 = (String) objects5.get("qNo");
+		String question5 = (String) objects5.get("question");
+		String option5 = (String) objects5.get("option");
+		String result5 = (String) objects5.get("result");
+		String type5 = (String) objects5.get("type");
+		System.out.println("question: " + objective5 + " " + qNo5 + " " + question5 + " " + option5 + " "
+				+ result5 + " " + type5);
+		q2.setqNo5(Integer.parseInt(qNo5));
+		q2.setQ5(question5);
+		q2.setQa5(option5);
+		q2.setRestult5(result5);
+		q2.setType5(type5);
+
+		JSONObject objects6 = getArray.getJSONObject(5);
+
+//			Question q2 = new Question();
+		String objective6 = (String) objects6.get("objective");
+		String qNo6 = (String) objects6.get("qNo");
+		String question6 = (String) objects6.get("question");
+		String option6 = (String) objects6.get("option");
+		String result6 = (String) objects6.get("result");
+		String type6 = (String) objects6.get("type");
+		System.out.println("question: " + objective6 + " " + qNo6 + " " + question6 + " " + option6 + " "
+				+ result6 + " " + type6);
+		q2.setqNo6(Integer.parseInt(qNo6));
+		q2.setQ6(question6);
+		q2.setQa6(option6);
+		q2.setRestult6(result6);
+		q2.setType6(type6);
+
+		JSONObject objects7 = getArray.getJSONObject(6);
+
+//			Question q2 = new Question();
+		String objective7 = (String) objects7.get("objective");
+		String qNo7 = (String) objects7.get("qNo");
+		String question7 = (String) objects7.get("question");
+		String option7 = (String) objects7.get("option");
+		String result7 = (String) objects7.get("result");
+		String type7 = (String) objects7.get("type");
+		System.out.println("question: " + objective7 + " " + qNo7 + " " + question7 + " " + option7 + " "
+				+ result7 + " " + type7);
+		q2.setqNo7(Integer.parseInt(qNo7));
+		q2.setQ7(question7);
+		q2.setQa7(option7);
+		q2.setRestult7(result7);
+		q2.setType7(type7);
+
+		JSONObject objects8 = getArray.getJSONObject(7);
+
+//			Question q2 = new Question();
+		String objective8 = (String) objects8.get("objective");
+		String qNo8 = (String) objects8.get("qNo");
+		String question8 = (String) objects8.get("question");
+		String option8 = (String) objects8.get("option");
+		String result8 = (String) objects8.get("result");
+		String type8 = (String) objects8.get("type");
+		System.out.println("question: " + objective8 + " " + qNo8 + " " + question8 + " " + option8 + " "
+				+ result8 + " " + type8);
+		q2.setqNo8(Integer.parseInt(qNo8));
+		q2.setQ8(question8);
+		q2.setQa8(option8);
+		q2.setRestult8(result8);
+		q2.setType8(type8);
+//			q2.setqNo(qNo);
+//			q2.setOptions(option);
+//			q2.setQuestion(question);
+//			q2.setResult(result);
+//			q2.setType(type);
+//			q2.setMobile((long) 3242342);
+//			listquestion.add(q2);
+
+//			}
+
+//		}
 		UserGame user = null;
 		try {
-			user = userService.addUserGame(userGame);
+			user = userService.addUserGame(q2);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/kgate/entity/UserGame.java
+++ src/main/java/com/kgate/entity/UserGame.java
@@ -18,9 +18,42 @@ public class UserGame {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long gameId;
-	
+
+	private String feedback;
+	private String duration;
+	private String dateTime;
+	private String percentage;
+	private String objective;
+
+	private String restult1;
+	private String restult2;
+	private String restult3;
+	private String restult4;
+	private String restult5;
+	private String restult6;
+	private String restult7;
+	private String restult8;
+
+	private String type1;
+	private String type2;
+	private String type3;
+	private String type4;
+	private String type5;
+	private String type6;
+	private String type7;
+	private String type8;
+
+	private int qNo1;
+	private int qNo2;
+	private int qNo3;
+	private int qNo4;
+	private int qNo5;
+	private int qNo6;
+	private int qNo7;
+	private int qNo8;
+
 	String mobile;
-	
+
 	private String gameName;
 //	@DateTimeFormat(pattern="dd-mm-yy")
 	@Temporal(TemporalType.DATE)
@@ -57,6 +90,238 @@ public class UserGame {
 	private String statusA8;
 
 	private Double score;
+
+	public String getFeedback() {
+		return feedback;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public String getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public String getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(String percentage) {
+		this.percentage = percentage;
+	}
+
+	public String getObjective() {
+		return objective;
+	}
+
+	public void setObjective(String objective) {
+		this.objective = objective;
+	}
+
+	public String getRestult1() {
+		return restult1;
+	}
+
+	public void setRestult1(String restult1) {
+		this.restult1 = restult1;
+	}
+
+	public String getRestult2() {
+		return restult2;
+	}
+
+	public void setRestult2(String restult2) {
+		this.restult2 = restult2;
+	}
+
+	public String getRestult3() {
+		return restult3;
+	}
+
+	public void setRestult3(String restult3) {
+		this.restult3 = restult3;
+	}
+
+	public String getRestult4() {
+		return restult4;
+	}
+
+	public void setRestult4(String restult4) {
+		this.restult4 = restult4;
+	}
+
+	public String getRestult5() {
+		return restult5;
+	}
+
+	public void setRestult5(String restult5) {
+		this.restult5 = restult5;
+	}
+
+	public String getRestult6() {
+		return restult6;
+	}
+
+	public void setRestult6(String restult6) {
+		this.restult6 = restult6;
+	}
+
+	public String getRestult7() {
+		return restult7;
+	}
+
+	public void setRestult7(String restult7) {
+		this.restult7 = restult7;
+	}
+
+	public String getRestult8() {
+		return restult8;
+	}
+
+	public void setRestult8(String restult8) {
+		this.restult8 = restult8;
+	}
+
+	public String getType1() {
+		return type1;
+	}
+
+	public void setType1(String type1) {
+		this.type1 = type1;
+	}
+
+	public String getType2() {
+		return type2;
+	}
+
+	public void setType2(String type2) {
+		this.type2 = type2;
+	}
+
+	public String getType3() {
+		return type3;
+	}
+
+	public void setType3(String type3) {
+		this.type3 = type3;
+	}
+
+	public String getType4() {
+		return type4;
+	}
+
+	public void setType4(String type4) {
+		this.type4 = type4;
+	}
+
+	public String getType5() {
+		return type5;
+	}
+
+	public void setType5(String type5) {
+		this.type5 = type5;
+	}
+
+	public String getType6() {
+		return type6;
+	}
+
+	public void setType6(String type6) {
+		this.type6 = type6;
+	}
+
+	public String getType7() {
+		return type7;
+	}
+
+	public void setType7(String type7) {
+		this.type7 = type7;
+	}
+
+	public String getType8() {
+		return type8;
+	}
+
+	public void setType8(String type8) {
+		this.type8 = type8;
+	}
+
+	public int getqNo1() {
+		return qNo1;
+	}
+
+	public void setqNo1(int qNo1) {
+		this.qNo1 = qNo1;
+	}
+
+	public int getqNo2() {
+		return qNo2;
+	}
+
+	public void setqNo2(int qNo2) {
+		this.qNo2 = qNo2;
+	}
+
+	public int getqNo3() {
+		return qNo3;
+	}
+
+	public void setqNo3(int qNo3) {
+		this.qNo3 = qNo3;
+	}
+
+	public int getqNo4() {
+		return qNo4;
+	}
+
+	public void setqNo4(int qNo4) {
+		this.qNo4 = qNo4;
+	}
+
+	public int getqNo5() {
+		return qNo5;
+	}
+
+	public void setqNo5(int qNo5) {
+		this.qNo5 = qNo5;
+	}
+
+	public int getqNo6() {
+		return qNo6;
+	}
+
+	public void setqNo6(int qNo6) {
+		this.qNo6 = qNo6;
+	}
+
+	public int getqNo7() {
+		return qNo7;
+	}
+
+	public void setqNo7(int qNo7) {
+		this.qNo7 = qNo7;
+	}
+
+	public int getqNo8() {
+		return qNo8;
+	}
+
+	public void setqNo8(int qNo8) {
+		this.qNo8 = qNo8;
+	}
 
 	public Long getGameId() {
 		return gameId;
@@ -290,18 +555,6 @@ public class UserGame {
 		this.score = score;
 	}
 
-	@Override
-	public String toString() {
-		return "UserGame [gameId=" + gameId + ", gameName=" + gameName + ", played=" + played
-				+ ", starPoints=" + starPoints + ", q1=" + q1 + ", q2=" + q2 + ", q3=" + q3
-				+ ", q4=" + q4 + ", q5=" + q5 + ", q6=" + q6 + ", q7=" + q7 + ", q8=" + q8
-				+ ", qa1=" + qa1 + ", qa2=" + qa2 + ", qa3=" + qa3 + ", qa4=" + qa4 + ", qa5="
-				+ qa5 + ", qa6=" + qa6 + ", qa7=" + qa7 + ", qa8=" + qa8 + ", statusA1="
-				+ statusA1 + ", statusA2=" + statusA2 + ", statusA3=" + statusA3 + ", statusA4="
-				+ statusA4 + ", statusA5=" + statusA5 + ", statusA6=" + statusA6 + ", statusA7="
-				+ statusA7 + ", statusA8=" + statusA8 + ", score=" + score + "]";
-	}
-
 	public String getMobile() {
 		return mobile;
 	}
@@ -309,7 +562,27 @@ public class UserGame {
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "UserGame [gameId=" + gameId + ", feedback=" + feedback + ", duration=" + duration
+				+ ", dateTime=" + dateTime + ", percentage=" + percentage + ", objective="
+				+ objective + ", restult1=" + restult1 + ", restult2=" + restult2
+				+ ", restult3=" + restult3 + ", restult4=" + restult4 + ", restult5=" + restult5
+				+ ", restult6=" + restult6 + ", restult7=" + restult7 + ", restult8=" + restult8
+				+ ", type1=" + type1 + ", type2=" + type2 + ", type3=" + type3 + ", type4="
+				+ type4 + ", type5=" + type5 + ", type6=" + type6 + ", type7=" + type7
+				+ ", type8=" + type8 + ", qNo1=" + qNo1 + ", qNo2=" + qNo2 + ", qNo3=" + qNo3
+				+ ", qNo4=" + qNo4 + ", qNo5=" + qNo5 + ", qNo6=" + qNo6 + ", qNo7=" + qNo7
+				+ ", qNo8=" + qNo8 + ", mobile=" + mobile + ", gameName=" + gameName
+				+ ", played=" + played + ", starPoints=" + starPoints + ", q1=" + q1 + ", q2="
+				+ q2 + ", q3=" + q3 + ", q4=" + q4 + ", q5=" + q5 + ", q6=" + q6 + ", q7=" + q7
+				+ ", q8=" + q8 + ", qa1=" + qa1 + ", qa2=" + qa2 + ", qa3=" + qa3 + ", qa4="
+				+ qa4 + ", qa5=" + qa5 + ", qa6=" + qa6 + ", qa7=" + qa7 + ", qa8=" + qa8
+				+ ", statusA1=" + statusA1 + ", statusA2=" + statusA2 + ", statusA3=" + statusA3
+				+ ", statusA4=" + statusA4 + ", statusA5=" + statusA5 + ", statusA6=" + statusA6
+				+ ", statusA7=" + statusA7 + ", statusA8=" + statusA8 + ", score=" + score
+				+ "]";
+	}
 
 }
